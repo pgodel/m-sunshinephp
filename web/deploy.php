7a446cd5d4f87ff07cd62a03dc5b85f3e63758e7
Pablo Godel <pgodel@gmail.com>
2013-01-26 18:42:45 -0500

    added deploy script for github webhook

---
+++ web/deploy.php
@@ -0,0 +1,4 @@
+<?php
+
+exec('/usr/bin/env -i HOME=/var/www/vhosts/m.sunshinephp.com/m-sunshine git pull origin master');
+//exec('/usr/bin/env -i HOME=/var/www/vhosts/m.sunshinephp.com/m-sunshine composer.phar install');
