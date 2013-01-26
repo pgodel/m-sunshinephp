e9e0c4d33a2b2ab74846b1617ceb1b928fe0e98d
Pablo Godel <pgodel@gmail.com>
2013-01-26 18:23:42 -0500

    removed old files

--- web/_footer.php
+++
@@ -1,9 +0,0 @@
-<div data-role="footer" data-id="menu" data-position="fixed">
-	<div data-role="navbar">
-		<ul>
-			<li><a href="#sessions" >Sessions</a></li>
-			<li><a href="#speakers" >Speakers</a></li>
-			<li><a href="http://sunshine.php/">Website</a></li>
-		</ul>
-	</div><!-- /navbar -->
-</div><!-- /footer -->
--- web/_header.php
+++
@@ -1,3 +0,0 @@
-<div data-role="header">
-	<h1>SunshinePHP 2013</h1>
-</div><!-- /header -->
